509d884fe9eaf3255bcb873aa61bb512bf6e09df
askula3 <askula3@github.com>
2021-10-01 12:40:29 +0200

    project init

---
+++ src/test/java/ba/bac_server/BacServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ba.bac_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BacServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
